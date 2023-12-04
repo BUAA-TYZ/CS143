9cd194b5a3428a8c36c11e3b13e93a5de0365ac6
BUAA-TYZ <2506741037@qq.com>
2023-12-04 21:35:59 +0800

    PA2 | TEST SCRIPT

---
+++ assignments/PA2/test.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+# test_compare.py
+import os
+import sys
+myOutput = os.popen("make dotest").read()
+stdOutput = os.popen("lexer test.cl").read()
+
+beginIndex = myOutput.index("#name")
+myOutput = myOutput[beginIndex:]
+
+while True:
+    if len(myOutput) == 0:
+      print("Success.")
+      sys.exit()
+    myEnd = myOutput.index("\n")
+    stdEnd = stdOutput.index("\n")
+    if myOutput[0 : myEnd] != stdOutput[0 : stdEnd]:
+        print("my flex ", myOutput[0 : myEnd])
+        print("std flex", stdOutput[0 : stdEnd])
+        print("")
+    myOutput = myOutput[myEnd + 1 :]
+    stdOutput = stdOutput[stdEnd + 1 :]
